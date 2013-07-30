71d29f01ad06512fbf6157d2e51a52ed51f159fa
extra.pedestrian@gmail.com <extra.pedestrian@gmail.com@175392c5-ee03-fad4-3d74-42a1f53baae1>
2013-07-30 14:48:26 +0000

    loading pickables from array and drawing them on map :) so far just one tost appeared :)

---
+++ trunk/source/game_object.js
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+//define basic properties for 3d object in game - wether its a monster, or pickable or container..
+function create_game_object () {
+
+	var gobject = new Object();
+	
+	gobject.position = new THREE.Vector3(0, 0, 0);
+	gobject.rotation = new THREE.Vector3(0, 0, 0);
+	gobject.scale = new THREE.Vector3(1, 1, 1);
+	
+	gobject.name = "unnamed";
+	gobject.description = "unnamed";
+	gobject.model = "unnamed";
+	gobject.icon = "unnamed";
+	gobject.id = 0;
+	
+	gobject.mesh = 0;
+
+	gobject.name_id = 0;
+	gobject.description_id = 0;
+	gobject.loadObject = loadObject;
+	
+	return gobject;
+}
+
+//load 3d mesh callback function
+function loadObject( gobject ) {
+	return function (geometry, materials ) {
+
+		alert("pera object loader" + gobject.id);
+		materials[ 0 ].shading = THREE.FlatShading;
+		gobject.mesh = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial( materials ) );
+		gobject.mesh.position.x = gobject.position.x;
+		gobject.mesh.position.z = gobject.position.z;
+		gobject.mesh.position.y = 1;
+		scene.add( gobject.mesh );
+	}
+}
---
+++ trunk/source/pickables.js
@@ -0,0 +1,22 @@
+
+
+// id, name, model, x, z
+pickables_array = [[1,"dagger","dagger.js", 2,5], [2,"holy symbol","models/tost.js", 6,4]];
+
+//load pickable 3d models on the map
+function load_pickables () {
+
+	var loader = new THREE.JSONLoader();
+	
+	for(var i=0; i<pickables_array.length; i++) {
+		var picki = create_game_object();
+		picki.id = pickables_array[i][0];
+		picki.name = pickables_array[i][1];
+		picki.model = pickables_array[i][2];
+		picki.position.x = pickables_array[i][3]*SQUARE_SIZE;
+		picki.position.z = pickables_array[i][4]*SQUARE_SIZE;
+		loader.load( picki.model, picki.loadObject(picki) );
+	}
+
+	alert("pera1");
+}
