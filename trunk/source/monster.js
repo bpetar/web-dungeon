f2be172575eadde894531ab22fe8c745c815bae7
extra.pedestrian@gmail.com <extra.pedestrian@gmail.com@175392c5-ee03-fad4-3d74-42a1f53baae1>
2013-09-21 21:43:07 +0000

    adding some monster class test code..

--- trunk/source/monster.js
+++ trunk/source/monster.js
@@ -2,3 +2,27 @@
 //monster class, attributes and methods
 
 //class declaration
+
+Monster = function ( ) {
+
+	// API
+
+	this.hp = 100; // milliseconds
+	this.ac = 40;
+	this.thaco = 30;
+	this.idle_startKeyframe = 0;
+	this.idle_endKeyframe = 90;
+	this.attack_startKeyframe = 90;
+	this.attack_endKeyframe = 140;
+	this.walk_startKeyframe = 140;
+	this.walk_endKeyframe = 180;
+	this.position = new THREE.Vector3(40, 0, 10);
+};
+
+Monster.prototype = Object.create(  );
+
+Monster.prototype.damage = function ( dmg ) {
+
+	this.hp -= dmg;
+
+};
