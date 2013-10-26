2e8645d1ce575db90ebb0cfbcc82db893a613c19
extra.pedestrian@gmail.com <extra.pedestrian@gmail.com@175392c5-ee03-fad4-3d74-42a1f53baae1>
2013-10-26 00:31:44 +0000

    monster steps aside if given proper item.

--- source/niche.js
+++ source/niche.js
@@ -1,7 +1,7 @@
 
 
 // niches and their content..
-var niche_pickables_array1 = [[2,"holy symbol","models/tost.js", "media/holy.png"]];// id, name, model, icon
+var niche_pickables_array1 = [[4,"holy symbol","models/tost.js", "media/holy.png"]];// id, name, model, icon
 //var niche_pickables_array2 = [[1,"healing","models/healing.js", "media/potion.png"], [2,"healing","models/healing.js", "media/potion.png"], [3,"healing","models/healing.js", "media/potion.png"]];// id, name, model, icon
 var nicheArr = [[3,5,1,niche_pickables_array1]]; //x,z,rot,content
 
