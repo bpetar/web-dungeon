7f938a647add7b24bcca54808f68e83d450c4863
extra.pedestrian@gmail.com <extra.pedestrian@gmail.com@175392c5-ee03-fad4-3d74-42a1f53baae1>
2013-11-12 16:31:44 +0000

    adding particles..

---
+++ trunk/source/particles.js
@@ -0,0 +1,90 @@
+
+
+// particles stuff
+
+
+//this goes to map file
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+//this stays in particles.js
+var ggeometry = 0;
+
+//load teleport particles
+function load_teleport()
+{
+	//teleport parameters could be on the map file
+	//position on map
+	var pos = new THREE.Vector3(175, 0, 105);
+	//color
+	
+
+	//load particles
+	
+	/*var material = new THREE.ParticleBasicMaterial( { map: new THREE.Texture( generateSprite() ), blending: THREE.AdditiveBlending } );
+
+	for ( var i = 0; i < 1000; i++ ) {
+
+		particle = new THREE.Particle( material );
+		
+		particle.position.x = Math.random() * 10 + pos.x;
+		particle.position.y = Math.random() * 10;
+		particle.position.z = Math.random() * 200;
+		particle.scale.x = particle.scale.y = Math.random() * 3 + 1;
+	
+		scene.add( particle );
+	}*/
+	
+	ggeometry = new THREE.Geometry();
+	var ssprite = THREE.ImageUtils.loadTexture( "media/particle.png" );
+	for ( i = 0; i < 500; i ++ ) {
+		var vertex = new THREE.Vector3();
+		vertex.x = 10 * Math.random() + pos.x;
+		vertex.y = 10 * Math.random();
+		vertex.z = 10 * Math.random() + pos.z;
+		vertex.velocity = new THREE.Vector3(0, Math.random()*0.1, 0); //y going up
+		ggeometry.vertices.push( vertex );
+	}
+	var mmaterial = new THREE.ParticleBasicMaterial( { size: 16, sizeAttenuation: false, map: ssprite, transparent: true } );
+	teleport = new THREE.ParticleSystem( ggeometry, mmaterial );
+	teleport.sortParticles = true;
+	scene.add( teleport );
+}
+
+function update_teleport()
+{
+	if(teleport != 0)
+	{
+		//teleport.rotation.y += 0.01;
+		
+		
+		var pCount = 500;
+		while(pCount--) {
+
+			// get the particle
+			var particle = ggeometry.vertices[pCount];
+
+			// check if we need to reset
+			if(particle.y > 10) {
+			  particle.y = 0;
+			  particle.velocity.y = 0;
+			}
+
+			// update the velocity with
+			// a splat of randomniz
+			particle.velocity.y +=  Math.random() * .01;
+
+			// and the position
+			particle.y += particle.velocity.y;
+		  }
+  
+		teleport.geometry.__dirtyVertices = true;
+	}
+}
+
+
+
+
+
