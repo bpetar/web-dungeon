e73987deccb9c973317dcbf4a0584d41e4062287
extra.pedestrian@gmail.com <extra.pedestrian@gmail.com@175392c5-ee03-fad4-3d74-42a1f53baae1>
2013-11-19 15:50:50 +0000

    using pickable items (healing, scroll..) with right click

--- trunk/source/pickables.js
+++ trunk/source/pickables.js
@@ -1,7 +1,22 @@
 
+//this goes in the map file:
 
-// id, name, model, x, z, icon
-var pickables_array = [[2,"rock","models/rocky.js", 12,2, "media/rock.png"], [3,"healing","models/healing.js", 9,0, "media/potion.png"]];
+function healingScript()
+{
+	console.log("Healing is being drinked!");
+	playerHPcurrent += 15;
+	if (playerHPcurrent > playerHPmax)
+	{
+		playerHPcurrent = playerHPmax;
+	}
+}
+
+// id, name, model, x, z, icon, useage hint, use script, consumable
+var pickables_array = [[2,"rock","models/rocky.js", 12,2, "media/rock.png", "This is too hard to chew.."], [3,"healing","models/healing.js", 9,0, "media/potion.png", "Player drinks healing potion", healingScript, 1]];
+
+
+
+//this stays in the file:
 
 //this is array of pickables on the ground, but also of those lying in the niches (added in niche.js)
 var array_of_pickables = [];
@@ -20,12 +35,21 @@ function load_pickables () {
 		picki.position.z = pickables_array[i][4]*SQUARE_SIZE+4;
 		picki.position.y = 0;
 		picki.icon = pickables_array[i][5];
+		picki.useHint = pickables_array[i][6];
+		if(pickables_array[i].length>7)
+		{
+			console.log("pickable script function being loaded: " + picki.name);
+			picki.useScript = pickables_array[i][7];
+		}
+		if(pickables_array[i].length>8)
+		{
+			picki.consumable = (pickables_array[i][8] == 1)?true:false;
+		}
 		picki.niched = -1; //flag indicating if pickable is in the niche
-		//alert("pera " + picki.model);
+
 		loader.load( picki.model, picki.loadObject(picki) );
 		
 		array_of_pickables.push(picki);
 	}
 
-	//alert("pera1");
 }
