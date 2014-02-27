4a14f784f9a07c12c1669fafc3c04aed2b8812ef
extra.pedestrian@gmail.com <extra.pedestrian@gmail.com@175392c5-ee03-fad4-3d74-42a1f53baae1>
2014-02-27 16:02:08 +0000

    level editor is working amazingly already. you can click on the little squares and it instantly updates the 3d view of the dungeon... im so good at this.

--- trunk/cubereditor.php
+++ trunk/cubereditor.php
@@ -63,8 +63,7 @@
 	grid
 		<div id='grid_left_indent' style='height:100%; width:50px; float: left;'>
 		</div>
-		<div id='grid_view' style='height:100%; float: left; border:1px solid yellow;'>
-		view
+		<div id='grid_view' style='height:100%; background:url(media/map.png); float: left; border:1px solid yellow;'>
 		</div>
 		<div id='grid_elements' style='float: left; border:1px solid blue;'>
 		elements
@@ -79,19 +78,268 @@
 	
 	var camera, scene, renderer;
 
+	var TILE_EMPTY = 0;
+	var TILE_FLOOR = 1;
+	var TILE_DOOR = 2;
+	
 	var wall_object = 0;
 	var floor_object = 0;
 	var ceiling_object = 0;
+	var floor_model = "";
+	var floor_texture_file = 'maps/level1/media/floor_11_1.png';
+	var wall_texture_file = 'maps/level1/media/stone_wall_01_01.png';
+	var ceiling_texture_file = 'maps/level1/media/ceiling.png';
 	
+	var SQUARE_SIZE = 10;
 	var grid_size = 20;
 	var grid_container = document.getElementById( 'grid_view' );
+	var element_at_hand = TILE_FLOOR;
+	var current_position = new THREE.Vector3(10,3,2); //x,z,up
 	
+	var scenObjArr = [];
+	var mapArr = new Array(grid_size);
+	var floorsArr2D = [];
+	var oldFloorsArr2D = [];
+	var mousePressed = false;
+	
+	function loadModel(model_object) {
+		return function (geometry, materials ) {
+			materials[ 0 ].shading = THREE.SmoothShading;
+			model_object = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial( materials ) );
+		}
+	}
+			
 	function load_models()
 	{
+		var loader = new THREE.JSONLoader();
 		//load models once, clone them later
-		wall_object = 0;
-		floor_object = 0;
-		ceiling_object = 0;
+	
+		//loader.load( floor_model, loadModel(floor_object) );
+		var map = THREE.ImageUtils.loadTexture( floor_texture_file );
+		map.wrapS = map.wrapT = THREE.RepeatWrapping;
+		map.anisotropy = 16;
+		var floor_material = new THREE.MeshLambertMaterial( { ambient: 0xbbbbbb, map: map, side: THREE.DoubleSide } );
+		floor_object = new THREE.Mesh( new THREE.PlaneGeometry( SQUARE_SIZE, SQUARE_SIZE, 1, 1 ), floor_material );
+		
+		//loader.load( wall_model, loadModel(wall_object) );
+		var map_wall = THREE.ImageUtils.loadTexture( wall_texture_file );
+		map_wall.wrapS = map_wall.wrapT = THREE.RepeatWrapping;
+		map_wall.anisotropy = 16;
+		var material_wall = new THREE.MeshLambertMaterial( { ambient: 0xbbbbbb, map: map_wall, side: THREE.DoubleSide } );
+		wall_object = new THREE.Mesh( new THREE.PlaneGeometry( SQUARE_SIZE, SQUARE_SIZE, 1, 1 ), material_wall );
+		
+		//loader.load( ceiling_model, loadModel(ceiling_object) );
+		var map_ceiling = THREE.ImageUtils.loadTexture( ceiling_texture_file );
+		map_ceiling.wrapS = map_ceiling.wrapT = THREE.RepeatWrapping;
+		map_ceiling.anisotropy = 16;
+		var material_ceiling = new THREE.MeshLambertMaterial( { ambient: 0xbbbbbb, map: map_ceiling, side: THREE.DoubleSide } );
+		ceiling_object = new THREE.Mesh( new THREE.PlaneGeometry( SQUARE_SIZE, SQUARE_SIZE, 1, 1 ), material_ceiling );
+		//loader.load( hole_model, loadModel(hole_object) );
+	}
+	
+	//to be called on every change
+	function createFloorArrFromMap()
+	{
+		oldFloorsArr2D = floorsArr2D.slice(0);
+		floorsArr2D.length = 0;
+		
+		for (var i=0; i< grid_size; i++)
+		{
+			for (var j=0; j< grid_size; j++)
+			{
+				if(mapArr[i][j].type != 0)
+				{
+					floorsArr2D.push([grid_size-j,grid_size-i]);
+				}
+			}
+		}
+		console.log(floorsArr2D);
+		
+		createDungeonFromFloorArr();
+	}
+
+	//create dungeon fromFloorArr
+	function createDungeonFromFloorArr()
+	{
+		/*for (var k=0; k<oldFloorsArr2D.length; k++)
+		{
+			var deleted = true;
+			
+			for (var m=0; m<floorsArr2D.length; m++)
+			{
+				if((floorsArr2D[i][0] == oldFloorsArr2D[k][0])&&(floorsArr2D[i][1] == oldFloorsArr2D[k][1]))
+				{
+					deleted = false;
+				}
+			}
+			
+			if(deleted)
+			{
+				//remove tile from scene
+			}
+			
+		}*/
+
+		//remove all
+		for (var o=1; o<scenObjArr.length; o++)
+		{
+			scene.remove(scenObjArr[o]);
+			//scenObjArr[o].deallocate();
+		}
+		
+		for (var i=0; i<floorsArr2D.length; i++)
+		{
+			var floor_tile_already_exist = false;
+			
+			for (var j=0; j<oldFloorsArr2D.length; j++)
+			{
+				if((floorsArr2D[i][0] == oldFloorsArr2D[j][0])&&(floorsArr2D[i][1] == oldFloorsArr2D[j][1]))
+				{
+					floor_tile_already_exist = true;
+				}
+			}
+			
+			//if(!floor_tile_already_exist)
+			{
+				console.log("ugi");
+				//add floor tile to scene
+				var object = floor_object.clone();
+				object.rotation.set(-Math.PI/2, 0, 0);
+				object.position.x = floorsArr2D[i][0]*SQUARE_SIZE; //x
+				object.position.y = 0; //y
+				object.position.z = floorsArr2D[i][1]*SQUARE_SIZE; //z
+				scene.add( object );
+				
+				scenObjArr.push(object);
+				
+				
+				var leftWall = true;
+				var rightWall = true;
+				var frontWall = true;
+				var backWall = true;
+				var xTile = floorsArr2D[i][0];
+				var yTile = floorsArr2D[i][1];
+
+				for(j=0; j < floorsArr2D.length; j++)
+				{
+					if(i!=j)
+					{
+						if((floorsArr2D[j][0] == xTile+1) && (floorsArr2D[j][1] == yTile))
+						{
+							//there is floor tile to the left - no LeftWall.
+							leftWall = false;
+						}
+						if((floorsArr2D[j][0] == xTile-1) && (floorsArr2D[j][1] == yTile))
+						{
+							//there is floor tile to the right - no RightWall.
+							rightWall = false;
+						}
+						if((floorsArr2D[j][0] == xTile) && (floorsArr2D[j][1] == yTile+1))
+						{
+							//there is floor tile to the front - no FrontWall.
+							frontWall = false;
+						}
+						if((floorsArr2D[j][0] == xTile) && (floorsArr2D[j][1] == yTile-1))
+						{
+							//there is floor tile to the back - no BackWall.
+							backWall = false;
+						}
+					}
+				}
+				
+				if(leftWall)
+				{
+					var object = wall_object.clone();
+					object.rotation.set(0, Math.PI/2, 0);
+					object.position.x = (floorsArr2D[i][0]+0.5)*SQUARE_SIZE; //x
+					object.position.y = 0.4*SQUARE_SIZE; //y
+					object.position.z = (floorsArr2D[i][1])*SQUARE_SIZE; //z
+					scene.add( object );
+					scenObjArr.push(object);
+				}
+				if(rightWall)
+				{
+					var object = wall_object.clone();
+					object.rotation.set(0, Math.PI/2, 0);
+					object.position.x = (floorsArr2D[i][0]-0.5)*SQUARE_SIZE; //x
+					object.position.y = 0.4*SQUARE_SIZE; //y
+					object.position.z = (floorsArr2D[i][1])*SQUARE_SIZE; //z
+					scene.add( object );
+					scenObjArr.push(object);
+				}
+				if(frontWall)
+				{
+					var object = wall_object.clone();
+					object.rotation.set(0, Math.PI, 0);
+					object.receiveShadow = true;
+					object.position.x = floorsArr2D[i][0]*SQUARE_SIZE; //x
+					object.position.y = 0.4*SQUARE_SIZE; //y
+					object.position.z = (floorsArr2D[i][1]+0.5)*SQUARE_SIZE; //z
+					scene.add( object );
+					scenObjArr.push(object);
+				}
+				if(backWall)
+				{
+					var object = wall_object.clone();
+					object.rotation.set(0, Math.PI, 0);
+					object.receiveShadow = true;
+					object.position.x = floorsArr2D[i][0]*SQUARE_SIZE; //x
+					object.position.y = 0.4*SQUARE_SIZE; //y
+					object.position.z = (floorsArr2D[i][1]-0.5)*SQUARE_SIZE; //z
+					scene.add( object );
+					scenObjArr.push(object);
+				}
+			}
+		}
+	}
+
+		
+	function changeDivBorderColorOver(divy,i,j)
+	{
+		if(mapArr[i][j].type == 0)
+		{
+			if(mousePressed)
+			{
+				mapArr[i][j].type = element_at_hand;
+				divy.style.border = "1px solid black";
+				createFloorArrFromMap();
+			}
+			else
+			{
+				divy.style.border = "1px solid gray";
+			}
+		}
+	}
+	function changeDivBorderColorOutTimed(divy, i, j)
+	{
+		if(mapArr[i][j].type == 0)
+		{
+			divy.style.border = "1px solid transparent";
+		}
+	}
+	function changeDivBorderColorOut(divy,i,j)
+	{
+		if(mapArr[i][j].type == 0)
+		{
+			setTimeout(changeDivBorderColorOutTimed ,3000, divy, i, j);
+		}
+	}
+	function setDivField(divy,i,j)
+	{
+		//called when div is clicked
+		console.log("inhere.asdasd." + i + " " + j );
+		if(element_at_hand == mapArr[i][j].type)
+		{
+			mapArr[i][j].type = TILE_EMPTY; //floor, door, etc
+			divy.style.border = "1px solid transparent";
+			createFloorArrFromMap();
+		}
+		else
+		{
+			mapArr[i][j].type = element_at_hand; //floor, door, etc
+			divy.style.border = "1px solid black";
+			createFloorArrFromMap();
+		}
 	}
 	
 	function draw_grid() 
@@ -99,13 +347,54 @@
 		//create divs and initialize them
 		for(var i=0; i< grid_size; i++)
 		{
-			var grid_element = document.createElement("div");
-			grid_element.appendChild(document.createTextNode('O'));
-			grid_container.appendChild(grid_element);
+			for(var j=0; j< grid_size; j++)
+			{
+				var grid_element = document.createElement("div");
+				//console.log("inhere.asdasd.");
+				grid_element.onmouseover = function(arg1,arg2,arg3){return function(){changeDivBorderColorOver(arg1,arg2,arg3);}}(grid_element,i,j);
+				grid_element.onmouseout = function(arg1,arg2,arg3){return function(){changeDivBorderColorOut(arg1,arg2,arg3);}}(grid_element,i,j);
+				grid_element.onmousedown = function(arg1,arg2,arg3) { return function() { setDivField(arg1,arg2,arg3); }}(grid_element,i,j);
+				grid_element.style.border = "1px solid transparent";
+				grid_element.style.width = "" + (grid_container.offsetHeight/grid_size-2) + "px";
+				grid_element.style.height = "" + (grid_container.offsetHeight/grid_size-2) + "px";
+				grid_element.style.float = "left";
+				
+				//grid_element.appendChild(document.createTextNode('O'));
+				grid_container.appendChild(grid_element);
+				
+				mapArr[i][j] = new Object();
+				mapArr[i][j].div = grid_element;
+				mapArr[i][j].type = TILE_EMPTY; //init with empty square
+
+				if((j==10)&&(i==17))
+				{
+					grid_element.style.border = "1px solid black";
+					grid_element.innerHTML = "+";
+					mapArr[i][j].type = TILE_FLOOR; //init with empty square
+				}
+			}
+			
+			//???wtf
+			var grid_element_new_line = document.createElement("div");
+			grid_element_new_line.style.border = "1px solid transparent";
+			grid_element_new_line.style.width = "" + (grid_container.offsetHeight/grid_size-2) + "px";
+			grid_element_new_line.style.height = "" + (grid_container.offsetHeight/grid_size-2) + "px";
+			grid_container.appendChild(grid_element_new_line);
+
 		}
-		
 	}
 	
+	function onMouseDown ()
+	{
+		mousePressed = true;
+	}
+	
+	function onMouseUp ()
+	{
+		mousePressed = false;
+	}
+
+
 	function init()
 	{
 		//scene
@@ -119,14 +408,35 @@
 
 		//camera
 		camera = new THREE.PerspectiveCamera( 47, dungeon_container.offsetHeight / dungeon_container.offsetHeight, 1, 10000 );
-		camera.position.x = 160;
+		camera.position.x = 100;
 		camera.position.y = 4;
-		camera.position.z = -5; //115
-		camera.look = new THREE.Vector3(160,4,5); //160,4,125
+		camera.position.z = 25; //115
+		camera.look = new THREE.Vector3(100,4,35); //160,4,125
 		camera.lookAt(camera.look);
 		
+		var ambientLight = new THREE.AmbientLight( 0xf0f0f0 ); // soft white light
+		scene.add( ambientLight );
+
+		//init map array
+		for (var m=0; m<grid_size; m++)
+		{
+			mapArr[m] = new Array(grid_size);
+		}
+		
+		document.addEventListener( 'mousedown', onMouseDown, false );
+		document.addEventListener( 'mouseup', onMouseUp, false );
+
 		//init grid
 		draw_grid();
+		
+		//load models
+		load_models();
+		
+		//to be called on every change
+		createFloorArrFromMap();
+		
+		//create dungeon fromFloorArr
+		//createDungeonFromFloorArr();
 	}
 	
 	function animate() {
