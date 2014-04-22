65a80619411c40bcbab6bd6167b72799d226069c
extra.pedestrian@gmail.com <extra.pedestrian@gmail.com@175392c5-ee03-fad4-3d74-42a1f53baae1>
2014-04-22 22:58:16 +0000

    gui mockup

---
+++ index.php
@@ -0,0 +1,58 @@
+	<div id="gui" style="min-height:650px; min-width:950px; height:100%; width:100%; display:table;">
+	
+		<div id="gui_left" style="position:relative; background: url(media/gui/background.png); border:1px solid black; display:table-cell; height:100%; width:208px;">
+        
+            <div id="profiles" style="position:absolute; top:0; bottom:0; margin-bottom:64px; width:100%;">
+            
+                <div id="inventory" style="background: url(media/gui/background.png); width:100%; height:8px">
+                </div>
+                <div id="profiles" style="background: url(media/gui/profile.png) no-repeat; width:100%; height:200px; margin-left:8px; ">
+                </div>
+                <div id="inventory" style="background: url(media/gui/background.png); width:100%; height:8px; display:none;">
+                </div>
+                <div id="profiles" style="background: url(media/gui/profile.png) no-repeat; width:100%; height:200px; margin-left:8px; display:none;">
+                </div>
+                        
+			</div>
+
+			<div id="options" style="background:url(media/gui/buttons.png); position:absolute; bottom:0px; height:64px; width:100%;"><br><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;OPTIONS</b>
+			</div>
+            
+		</div>
+
+		<div id="gui_center" style="position:relative; display:table-cell; height:100%;">
+
+			<div id="container" style="background:url(media/gui/3dview.png); position:absolute; top:0; bottom:0; margin-bottom:64px; width:100%;">
+			</div>
+
+			<div id="console" style="background:url(media/gui/background.png); position:absolute; bottom:0px; color:yellow; height:64px; width:100%;">
+            Game Initialized.
+            <br>
+            Martin: I smell something funny in this corner..
+            <br>
+            Root picked up!
+			</div>
+		</div>
+        
+		<div id="gui_right" style="position:relative; display:table-cell; background: url(media/gui/background.png); border:1px solid black; width:208px; height:100%;">
+        
+            <div id="inventory" style="background: url(media/gui/background.png); width:192px; height:8px">
+			</div>
+            
+            <div id="inventory" style="background: url(media/gui/slot.png); width:192px; height:192px; margin-left:6px;">
+			</div>
+            
+            <div id="inventory" style="background: url(media/gui/background.png); width:192px; height:8px">
+			</div>
+            
+            <div id="inventory" style="background: url(media/gui/root.png); border:1px solid yellow; width:192px; height:192px; display:none;">
+			</div>
+            
+            <div id="inventory" style="background: url(media/gui/background.png); border:1px solid red; width:192px; height:8px; display:none;">
+			</div>
+                
+			<div id="map" style="background:url(media/gui/slot1.png); position:absolute; bottom:0px; height:64px; width:100%;">
+			</div>
+		</div>
+		
+	</div>
