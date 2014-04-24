70f4e8c560df3d0d68e0c71b9b07a40ef67fd1ac
extra.pedestrian@gmail.com <extra.pedestrian@gmail.com@175392c5-ee03-fad4-3d74-42a1f53baae1>
2014-04-24 23:58:25 +0000

    fixing gui on index some more..

--- trunk/index.php
+++ trunk/index.php
@@ -1,28 +1,28 @@
 	<div id="gui" style="min-height:650px; min-width:950px; height:100%; width:100%; display:table;">
 	
-		<div id="gui_left" style="position:relative; background: url(media/gui/background.png); border:1px solid black; display:table-cell; height:100%; width:208px;">
+		<div id="gui_left" style="position:relative; background: url(media/gui/background_up.png); display:table-cell; height:100%; width:200px;">
         
-            <div id="profiles" style="position:absolute; top:0; bottom:0; margin-bottom:64px; width:100%;">
+            <div id="profiles" style="position:absolute; top:0; bottom:0; margin-bottom:64px; width:100%; border:1px solid black;">
             
-                <div id="inventory" style="background: url(media/gui/background.png); width:100%; height:8px">
+                <div id="inventory" style="background: url(media/gui/background.png); width:100%; height:4px">
                 </div>
-                <div id="profiles" style="background: url(media/gui/profile.png) no-repeat; width:100%; height:200px; margin-left:8px; ">
+                <div id="profiles" style="background: url(media/gui/profile.png) no-repeat; width:100%; height:200px; margin-left:4px; ">
                 </div>
                 <div id="inventory" style="background: url(media/gui/background.png); width:100%; height:8px; display:none;">
                 </div>
-                <div id="profiles" style="background: url(media/gui/profile.png) no-repeat; width:100%; height:200px; margin-left:8px; display:none;">
+                <div id="profiles" style="background: url(media/gui/profile.png) no-repeat; width:100%; height:200px; margin-left:4px; display:none;">
                 </div>
                         
 			</div>
 
-			<div id="options" style="background:url(media/gui/buttons.png); position:absolute; bottom:0px; height:64px; width:100%;"><br><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;OPTIONS</b>
+			<div id="options" style="background:url(media/gui/buttons.png); background-size: 100% 100%; position:absolute; bottom:0px; cursor:pointer; height:64px; width:100%; text-align:center; line-height:64px;"><b>OPTIONS</b>
 			</div>
             
 		</div>
 
 		<div id="gui_center" style="position:relative; display:table-cell; height:100%;">
 
-			<div id="container" style="background:url(media/gui/3dview.png); position:absolute; top:0; bottom:0; margin-bottom:64px; width:100%;">
+			<div id="container" style="background:url(media/gui/3dview.png); position:absolute; top:0; bottom:0; margin-bottom:64px; width:100%; border:1px solid black;">
 			</div>
 
 			<div id="console" style="background:url(media/gui/background.png); position:absolute; bottom:0px; color:yellow; height:64px; width:100%;">
@@ -34,7 +34,7 @@
 			</div>
 		</div>
         
-		<div id="gui_right" style="position:relative; display:table-cell; background: url(media/gui/background.png); border:1px solid black; width:208px; height:100%;">
+		<div id="gui_right" style="position:relative; display:table-cell; background: url(media/gui/background_up.png); border:1px solid black; width:208px; height:100%;">
         
             <div id="inventory" style="background: url(media/gui/background.png); width:192px; height:8px">
 			</div>
@@ -51,7 +51,11 @@
             <div id="inventory" style="background: url(media/gui/background.png); border:1px solid red; width:192px; height:8px; display:none;">
 			</div>
                 
-			<div id="map" style="background:url(media/gui/slot1.png); position:absolute; bottom:0px; height:64px; width:100%;">
+			<div id="map" style="background:url(media/gui/background.png); position:absolute; bottom:0px; height:64px; width:100%;">
+                <div id="special_slot1" style="float:left; margin-right:2px; margin-left:4px; background: url(media/gui/slot1.png) no-repeat; width:64px; height:64px"></div>
+                <div id="special_slot2" style="float:left; margin-right:2px; margin-left:2px; background: url(media/gui/slot1.png) no-repeat; width:64px; height:64px"></div>
+                <div id="special_slot3" style="float:left; margin-right:4px; margin-left:2px; background: url(media/gui/slot1.png) no-repeat; width:64px; height:64px"></div>
+			</div>
 			</div>
 		</div>
 		
